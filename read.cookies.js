aab42ecebcfadc5ac9118d9394d7c6a36e7b90a5
jkherrick <jherrick5@capellauniversity.edu>
2025-08-27 14:21:37 -0400

    Add files via upload

---
+++ read.cookies.js
@@ -0,0 +1,10 @@
+/* Function to read cookies, get their name-value pairs, and display the name-value
+   pairs in the output area */
+const displayCookieValues = function() {
+
+}
+
+// Event handler called when page has loaded
+window.onload = () => {
+    // Add code here to call function to display cookie values
+}
